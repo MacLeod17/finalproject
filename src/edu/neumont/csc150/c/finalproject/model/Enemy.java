aa2eb7f07881651f94132478811a18d7d0ee36fd
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-03-01 06:03:28 -0700

    Character creation, saving, searching, and viewing all fully implemented; (de)serialization of Player data; added percentChanceToFollowPlayer to Enemy class

--- src/edu/neumont/csc150/c/finalproject/model/Enemy.java
+++ src/edu/neumont/csc150/c/finalproject/model/Enemy.java
@@ -2,25 +2,28 @@ package edu.neumont.csc150.c.finalproject.model;
 
 public class Enemy extends Character {
     private int gold;
+    private int percentChanceToFollowPlayer;
     private boolean despicableAct;
 
     public Enemy() {
 
     }
 
-    public Enemy(String name, int healthDice, int healthSides, int armorClass, int damageMod, int attackDice, int attackSides, int gold, boolean despicableAct) {
+    public Enemy(String name, int healthDice, int healthSides, int armorClass, int damageMod, int attackDice, int attackSides, int gold, int percentChanceToFollowPlayer, boolean despicableAct) {
         this.setName(name);
         this.setTotalHealth(this.roll(healthDice, healthSides));
-        /* Two different enemies of the same type can have different total health
+        this.setCurrentHealth(this.getTotalHealth());
+        /** Two different enemies of the same type can have different total health
         (Some individuals will be tougher than others of the same type) */
         this.setArmorClass(armorClass);
         this.setDamageMod(damageMod);
         this.setHitBonus(healthDice - 1);
-        //Basically sets hit bonus equal to number of Hit Dice (Monster equivalent of level); Bigger Bads attack better
+        /** Basically sets hit bonus equal to number of Hit Dice (Monster equivalent of level); Bigger Bads attack better */
         this.setAttackDice(attackDice);
         this.setAttackSides(attackSides);
 
         this.setGold(gold);
+        this.setPercentChanceToFollowPlayer(percentChanceToFollowPlayer);
         this.setDespicableAct(despicableAct);
     }
 
@@ -29,7 +32,22 @@ public class Enemy extends Character {
     }
 
     public void setGold(int gold) {
+        if (gold < 0) {
+            throw new IllegalArgumentException("gold must be non-negative");
+        }
         this.gold = gold;
+    }
+
+    /** Used if character decides to flee from battle */
+    public int getPercentChanceToFollowPlayer() {
+        return percentChanceToFollowPlayer;
+    }
+
+    public void setPercentChanceToFollowPlayer(int percentChanceToFollowPlayer) {
+        if (percentChanceToFollowPlayer < 0) {
+            throw new IllegalArgumentException("chance to follow player must be non-negative");
+        }
+        this.percentChanceToFollowPlayer = percentChanceToFollowPlayer;
     }
 
     public boolean isDespicableAct() {
