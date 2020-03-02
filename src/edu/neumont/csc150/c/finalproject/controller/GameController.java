aa8e98f15165bba270d0c800d54a60423d695c33
Corey Mundy <minusemerkat3@yahoo.com>
2020-03-02 09:42:20 -0700

    Changed town to menu system

--- src/edu/neumont/csc150/c/finalproject/controller/GameController.java
+++ src/edu/neumont/csc150/c/finalproject/controller/GameController.java
@@ -3,11 +3,29 @@ package edu.neumont.csc150.c.finalproject.controller;
 import edu.neumont.csc150.c.finalproject.model.Player;
 
 public class GameController {
-
     public void run(Player player) {
         //garrick
         //ggggggg
         //Can you see this?
         //Noah Was Here
+
+
     }
+    public boolean checkForMovementRight() {
+        return false;
+    }
+
+    public boolean checkForMovementLeft() {
+        return false;
+    }
+
+    public boolean checkForBackwardsMovement() {
+        return false;
+    }
+
+    public boolean checkForForwardMovement() {
+        return false;
+    }
+
 }
+
--- src/edu/neumont/csc150/c/finalproject/model/Town.java
+++ src/edu/neumont/csc150/c/finalproject/model/Town.java
@@ -2,40 +2,5 @@ package edu.neumont.csc150.c.finalproject.model;
 
 public class Town {
 
-    private int locationRow;
-    private int locationColumn;
-
-    public static final int TOWNSQUARE_ROWS = 10;
-    public static final int TOWNSQUARE_COLUMNS = 10;
-
-    public char[][] townSquares = new char[][]{
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'},
-            {'-','-','-','-','-','-','-','-','-','-'}
-    };
-
-
-    public boolean checkForMovementRight() {
-        return false;
-    }
-
-    public boolean checkForMovementLeft() {
-        return false;
-    }
-
-    public boolean checkForBackwardsMovement() {
-        return false;
-    }
-
-    public boolean checkForForwardMovement() {
-        return false;
-    }
 
 }
