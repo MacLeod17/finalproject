e15e1f9692958a551798a28661c3b019a8299d34
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-03-02 00:37:19 -0700

    playGame in MainMenuController implemented, but the actual game itself has yet to start implementation

--- src/edu/neumont/csc150/c/finalproject/model/Game.java
+++ src/edu/neumont/csc150/c/finalproject/model/Game.java
@@ -1,5 +1,9 @@
 package edu.neumont.csc150.c.finalproject.model;
 
 public class Game {
+    /** What the game checks */
+    char[][] gameMap = new char[15][15];
 
+    /** The map that is shown to the user, so that the terrain for each spot is not truly overwritten by the user's current location */
+    char[][] userMap = new char[15][15];
 }
