ce0ac5ed0b7ef052d056fc464118e41af7074314
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-04-24 23:28:14 -0600

    Removed unused import.

--- src/edu/neumont/csc150/c/finalproject/Launcher.java
+++ src/edu/neumont/csc150/c/finalproject/Launcher.java
@@ -1,7 +1,6 @@
 package edu.neumont.csc150.c.finalproject;
 
 import edu.neumont.csc150.c.finalproject.controller.MainMenuController;
-import edu.neumont.csc150.c.finalproject.view.MainMenuUI;
 
 import java.io.IOException;
 
